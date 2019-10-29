7a070e828aeb28523cfd51ac281a4e65832085b4
msidqi <mouadmr89@gmail.com>
2019-10-29 00:51:43 +0000

    neo4j connection

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -4,37 +4,34 @@ const port = (process.env.PORT || 3000);
 const express = require('express');
 const app = express();
 
-/*
+
 const db_user = 'neo4j';
 const db_passwd = 'neo4j';
 const neo4j = require('neo4j-driver').v1
-const driver = neo4j.driver('bolt://localhost', neo4j.auth.basic(db_user, db_passwd));
+const driver = neo4j.driver('bolt://database', neo4j.auth.basic(db_user, db_passwd));
 const session = driver.session();
-session
-  .run('MERGE (alice:Person {name : {nameParam} }) RETURN alice.name AS name', {
-    nameParam: 'Alice'
-  })
-  .subscribe({
-    onNext: function(record) {
-      console.log(record.get('name'))
-    },
-    onCompleted: function() {
-      session.close()
-    },
-    onError: function(error) {
-      console.log(error)
-    }
-  })
- 
+
+// Run a Cypher statement, reading the result in a streaming manner as records arrive:
+// session.run('MERGE (alice:Person {name : {nameParam} }) RETURN alice.name AS name', { nameParam: 'Alice' })
+//   .subscribe({
+//     onNext: function(record) {
+//       console.log(record.get('name'))
+//     },
+//     onCompleted: function() {
+//       session.close()
+//     },
+//     onError: function(error) {
+//       console.log(error)
+//     }
+//   })
+
 // or
 // the Promise way, where the complete result is collected before we act on it:
 session
-  .run('MERGE (james:Person {name : {nameParam} }) RETURN james.name AS name', {
-    nameParam: 'James'
-  })
+  .run('MATCH (n:Person) RETURN n LIMIT 25')
   .then(function(result) {
     result.records.forEach(function(record) {
-      console.log(record.get('name'))
+      console.log(record.get('n'))
     })
     session.close()
   })
@@ -42,7 +39,7 @@ session
     console.log(error)
   })
 
-*/
+
 
   
 
