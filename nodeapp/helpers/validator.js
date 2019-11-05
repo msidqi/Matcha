5f204b078dda79608ef3a4af6fbd74dd1f5ed7a4
msidqi <mouadmr89@gmail.com>
2019-11-05 03:01:05 +0100

    adding default values + validating rest of fields

--- nodeapp/helpers/validator.js
+++ nodeapp/helpers/validator.js
@@ -1,12 +1,10 @@
 
-
-
 const validateUsername = function (userName) {
     let err = "";
     
     if ((err = (typeof userName === 'string') ? "" : "Error: variable is not a string.") !== "")
 		return (err);
-	if ((err = userName.length === 0 ? "" : "Error : Please enter your username") !== "")
+	if ((err = userName.length !== 0 ? "" : "Error : Please enter your username") !== "")
 		return (err);
     if ((err = /^(.){6,}$/.test(userName) ? "" : "Username must be at least 6 characters.") !== "")
         return (err);
@@ -22,7 +20,7 @@ const validateName = function (flastName) {
     
     if ((err = (typeof flastName === 'string') ? "" : "Error: variable is not a string.") !== "")
         return (err);
-	if ((err = flastName.length === 0 ? "" : "Error : Please enter a name.") !== "")
+	if ((err = flastName.length !== 0 ? "" : "Error : Please enter a name.") !== "")
 		return (err);
 	if ((err = /^(.){4,}$/.test(flastName) ? "" : "Your name must be at least 4 characters.") !== "")
         return (err);
@@ -39,7 +37,7 @@ const validatePassword = function (password) {
     
     if ((err = (typeof password === 'string') ? "" : "Error: variable is not a string.") !== "")
 		return (err);
-	if ((err = password.length === 0 ? "" : "Error : Please enter a password.") !== "")
+	if ((err = password.length !== 0 ? "" : "Error : Please enter a password.") !== "")
 		return (err);
     if ((err = /^(?=.*[!-~])([!-~]){8,}$/.test(password) ? "" : "Password must be at least 8 characters.") !== "")
         return (err);
@@ -55,25 +53,31 @@ const validateEmail = function (email) {
     
     if ((err = (typeof email === 'string') ? "" : "Error: variable is not a string.") !== "")
 		return (err);
-	if ((err = email.length === 0 ? "" : "Error : Please enter an email.") !== "")
+	if ((err = email.length !== 0 ? "" : "Error : Please enter an email.") !== "")
 		return (err);
     if ((err = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email) ? "" : "Not a valide email address.") !== "")
         return (err);
     return (err);
 };
 
+const validateAge = function (age) {
+    let err = "";
+    
+    if ((err = (typeof age === 'number') ? "" : "Error: variable is not a number.") !== "")
+		return (err);
+	if ((err = (age >= 18) ? "" : "Error : User must be 18 or older.") !== "")
+		return (err);
+	if ((err = (age < 120) ? "" : "Error : BRUH go visit your grand children.") !== "")
+		return (err);
+    return (err);
+};
+
 const validateUser = function (user) {
-	let errors = {
-		username: "",
-		firstname: "",
-		lastname: "",
-		email: "",
-		password: ""
-	};
+	let errors = {};
 	let err = "";
 
     if ((err = validateUsername(user.username)) !== "")
-        errors.username = err;
+		errors.username = err;
     if ((err = validateName(user.firstname)) !== "")
 		errors.firstname = err;
     if ((err = validateName(user.lastname)) !== "")
@@ -82,42 +86,46 @@ const validateUser = function (user) {
 		errors.email = err;
     if ((err = validatePassword(user.password)) !== "")
 		errors.password = err;
-	for (e in errors) {
-		if (e !== "")
+	if ((err = validateAge(user.age)) !== "")
+		errors.age = err;
+	for (elem in errors) {
+		if (errors[elem] !== "")
 			throw errors;
 	}
 }
 
+
 const userFieldsExist = function (incomingUser, userFields) {
-	let err = 0;
+	let throwErr = false;
 	let errors = {};
 
+	if (typeof incomingUser === 'undefined' || incomingUser === null)
+		throw {error: 'user is undefined'};
 	userFields.username				= incomingUser.username;
 	userFields.firstname			= incomingUser.firstname;
 	userFields.lastname				= incomingUser.lastname;
+	userFields.password				= incomingUser.password;
 	userFields.email				= incomingUser.email;
 	userFields.age					= incomingUser.age;
-	userFields.score				= incomingUser.score;
-	userFields.location				= incomingUser.location;
-	userFields.gender				= incomingUser.gender;
-	userFields.sexualpreferences	= incomingUser.sexualpreferences;
-	userFields.biography			= incomingUser.biography;
-	userFields.pictures				= incomingUser.pictures;
-	userFields.interests			= incomingUser.interests;
 	for (let elem in userFields) {
 		if (typeof userFields[elem] === 'undefined' || userFields[elem] === null) {
 			errors[elem] = `field required.`;
-			err = 1;
+			throwErr = true;
 		}
 	}
-	if (err === 1)
+	if (throwErr === true)
 		throw errors;
 }
+
+
+const gender = ['male', 'female'];
+const sexualpreferences = ['str', 'bi', 'h'];
 
 module.exports = {
     username:			validateUsername,
     password:			validatePassword,
-    email:				validateEmail,
+	email:				validateEmail,
+	validateAge:		validateAge,
 	validateUser:		validateUser,
 	userFieldsExist:	userFieldsExist,
 }
